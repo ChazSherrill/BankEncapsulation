2aabb86e8da4d8ac28e575c6e881fcec605bd733
Chaz Sherrill <chazsherrill@icloud.com>
2020-09-01 19:57:07 -0500

    BAnkencaps done

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+   {
+        private double _balance;
+            
+        public BankAccount(double initialDeposit)
+        {
+            _balance = initialDeposit;
+        }
+
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                Balance = value;
+            }
+        }
+
+        public void deposit(double moneyIn)
+        {
+            _balance += moneyIn;
+        }
+
+        public double getBalance()
+        {
+            return Balance;
+        }
+
+        public void Withdraw(double moneyOut)
+        {
+            _balance -= moneyOut;
+            Console.WriteLine(Balance);
+        }
+        
+    }
+}
+
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var CLSAccount = new BankAccount(10);
+
+            CLSAccount.deposit(2273.32);
+
+            Console.WriteLine(CLSAccount.getBalance());
+
+            CLSAccount.Withdraw(200);
+        }
+    }
+}
